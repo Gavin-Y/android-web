0958f0113f8c93840b3d9b8b8b1bee17c0701a8f
u1095279147138 <1095279147138@tianchi.com>
2017-08-17 10:27:20 +0800

    一些小改动

--- app/src/main/java/com/gavin/alw/utils/WebApp/WebAppInterface.java
+++ app/src/main/java/com/gavin/alw/utils/WebApp/WebAppInterface.java
@@ -3,6 +3,8 @@ package com.gavin.alw.utils.WebApp;
 import android.content.Context;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
+import com.bigkoo.alertview.AlertView;
+import com.bigkoo.alertview.OnItemClickListener;
 
 /**
  * Create by Gavin_Y on 2017/8/16
@@ -22,6 +24,26 @@ public class WebAppInterface {
     @JavascriptInterface
     public void addView(){
 
+    }
+
+    @JavascriptInterface
+    public void showList(){
+        System.out.println("1");
+        new AlertView("筛选条件", null, "取消", new String[]{"查找关键字"},
+                new String[]{"最新评论", "按评分从高到低"},mContext,
+                AlertView.Style.ActionSheet, new OnItemClickListener() {
+            @Override
+            public void onItemClick(Object o, int position) {
+                switch (position){
+                    case 0:
+                        showToast("输入关键字");break;
+                    case 1:
+                        showToast("获取最新评论");break;
+                    case 2:
+                        showToast("获取评分高低");break;
+                }
+            }
+        }).show();
     }
 
 }
